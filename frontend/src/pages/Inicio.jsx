cf31fe620e32a55043ba82fd3a6e2e926f817bb3
fede-cid <fede.cid.96@gmail.com>
2023-01-27 00:26:11 -0300

    primera modificacion

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -11,51 +11,7 @@ const Inicio = () => {
       <EventoDestacado />
 
       <p>
-        Solo pa q haga bulto xd Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Omnis repellendus molestiae ipsum impedit nostrum,
-        similique incidunt labore molestias quasi aliquid magnam atque vero hic
-        ex nulla! Animi facilis reprehenderit doloribus? Modi, rerum excepturi?
-        Voluptate temporibus recusandae amet icabo et ullam adipisci omnis
-        quaerat fuga veniam, doloremque quod amet! Sapiente qui suscipit neque,
-        similique magni totam nobis! Totam onem ad! Similique, amet excepturi
-        fugit esse aut minima praesentium sunt sapiente mollitia aspernatur
-        possimus sed, nosdipisci delectus? Ullam, doloremque assumenda! Modi
-        praesentium dolorum amet explicabo unde temporibus eveniet ratione
-        nostrum inventore asperiores error dicta iure nulla, illo dignissimos
-        nesciunt omnis itaque perferendis quos sequi cupiditate ducimus animi
-        perspiciatis! Esse, quasi. At, blanditiis nostrum earum vero nihil esse
-        assumenda, numquam, sunt ipsum magni beatae cum ad quidem voluptatem
-        ipsa exercitationem fugit. Minus porro aut, libero eos id culpa neque
-        tenetur et? Obcaecati, illo totam! Harum, voluptatem? Ipsam eveniet
-        beatae nobis eaque exercitationem, harum impedit, unde saepe atque
-        deserunt tempora, dignissimos totam quisquam deleniti at reiciendis
-        molestiae autem quos! Qui, sapiente iusto? Ab nihil laborum fugiat, aut
-        neque magnam iste, distinctio sunt est fugit expedita. Aliquam veritatis
-        nam temporibus quam molestias ut harum repellendus sequi expedita ab,
-        adipisci mollitia nihil eum. Reprehenderit! Ea molestiae est eos aliquam
-        voluptate. Minus, corporis velit. Aut veniam unde excepturi cum
-        aspernatur aperiam? Cumque nihil, eius ex voluptate animi aperiam
-        temporibus natus exercitationem. Blanditiis harum suscipit sunt? Placeat
-        asperiores modi quibusdam! Provident ad, natus obcaecati ducimus
-        voluptatem cupiditate veniam omnis praesentium, quis fuga culpa nostrum
-        aspernatur aperiam eveniet reiciendis amet excepturi facilis doloribus a
-        esse! Est, adipisci. Illum, natus provident? Autem est accusamus ipsum
-        veniam explicabo id aperiam distinctio! Adipisci, amet id! Suscipit quia
-        temporibus, modi, incidunt porro aspernatur sunt id nesciunt eaque unde
-        aliquid quod fugit. Molestiae, delectus. Debitis ab, accusantium
-        consequuntur velit, corporis dolore quas, sapiente minus nemo tempora
-        ducimus ullam tenetur voluptatibus architecto a? Atque debitis
-        architecto beatae non ut earum? Doloribus, dolorem rerum? Fugit
-        provident quam eos impedit reprehenderit ullam, est corporis dicta
-        veniam dignissimos nemo eligendi eveniet ab architecto soluta quaerat
-        quos esse fuga? Similique quas non distinctio, dolores nisi voluptate
-        voluptates. Iusto cupiditate rerum beatae adipisci unde facere tenetur
-        quaerat saepe quidem nisi inventore ad veniam consequatur corrupti aut
-        voluptas eligendi, in ipsa quis voluptatem praesentium itaque ea.
-        Incidunt, vero perspiciatis? Rem asperiores magnam eum nam itaque
-        aliquam iste quam ipsam saepe quod quis, porro laborum officiis ex
-        accusamus id delectus praesentium, placeat numquam minima, nisi
-        voluptate minus error expedita? Nemo?
+   
       </p>
       <div
         style={{
@@ -72,7 +28,7 @@ const Inicio = () => {
         <button className="btnsecundario">Botón secundario</button>
         <button className="btnicono">Con Icono</button>
       </div>
-      
+
     </>
   );
 };
